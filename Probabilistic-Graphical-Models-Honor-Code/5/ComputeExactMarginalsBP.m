65990448e38dcf0726be7a654284615cda18adbe
Chao PAN <54287779+chaopan1995@users.noreply.github.com>
2020-06-01 15:46:02 +0200

    Create ComputeExactMarginalsBP.m

---
+++ Probabilistic-Graphical-Models-Honor-Code/5/ComputeExactMarginalsBP.m
@@ -0,0 +1,57 @@
+%COMPUTEEXACTMARGINALSBP Runs exact inference and returns the marginals
+%over all the variables (if isMax == 0) or the max-marginals (if isMax==1). 
+%
+%   M = COMPUTEEXACTMARGINALSBP(F, E, isMax) takes a list of factors F,
+%   evidence E, and a flag isMax, runs exact inference and returns the
+%   final marginals for the variables in the network. If isMax is 1, then
+%   it runs exact MAP inference, otherwise exact inference (sum-prod).
+%   It returns an array of size equal to the number of variables in the 
+%   network where M(i) represents the ith variable and M(i).val represents 
+%   the marginals of the ith variable. 
+%
+% Copyright (C) Daphne Koller, Stanford University, 2012
+
+
+function M = ComputeExactMarginalsBP(F, E, isMax)
+
+% initialization
+% you should set it to the correct value in your code
+M = [];
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% YOUR CODE HERE
+%
+% Implement Exact and MAP Inference.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+P = CreateCliqueTree(F, E);
+P = CliqueTreeCalibrate(P, isMax);
+
+all_var = [];
+for i = 1:length(P.cliqueList)
+    all_var = union(all_var, P.cliqueList(i).var);
+end;
+
+N = length(all_var);
+
+M = repmat(struct('var', [], 'card', [], 'val', []), N, 1);
+
+for i = 1:N
+    V = all_var(i);
+    for j = 1:length(P.cliqueList)
+        if ~isempty(intersect(P.cliqueList(j).var, V))
+            if isMax
+                M(i) = FactorMaxMarginalization(P.cliqueList(j),...
+                    setdiff(P.cliqueList(j).var, V));
+            else
+                
+                M(i) = FactorMarginalization(P.cliqueList(j),...
+                    setdiff(P.cliqueList(j).var, V));
+                M(i).val = M(i).val/sum(M(i).val);
+            end;
+            break;
+        end;
+    end;
+end;
+
+end
