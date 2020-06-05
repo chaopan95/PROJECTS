c4f612131ade813c6bc694474b8c8ca2515c4beb
Chao PAN <54287779+chaopan1995@users.noreply.github.com>
2020-06-05 11:59:37 +0200

    Create RecognizeUnknownActions.m

---
+++ Probabilistic-Graphical-Models-Honor-Code/9/RecognizeUnknownActions.m
@@ -0,0 +1,10 @@
+% You should put all your code for recognizing unknown actions in this
+% file.
+% Describe the method you used in YourMethod.txt.
+% Don't forget to call SavePrediction() at the end with your predicted
+% labels to save them for submission, then submit using submit.m
+
+
+% Read me
+% Using RecognizeActions, but give a datasetTest without labels, we can
+% add a dummy labels, such as ones(90, 1), finally we have a 13/20 note.
